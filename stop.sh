740e45d13a870c4b577bb49e77ddd17cb7fb3812
Raphaël Bunck <raphael@bunck.me>
2018-10-09 19:39:02 +0200

    Just trolling the repo (#9)

--- stop.sh
+++ stop.sh
@@ -1,11 +1,14 @@
+# Runs pkill uv4l 
 function camera() {
 pkill uv4l
 }
 
+# Stops the rover process
 function rover() {
 pm2 stop index
 }
 
+# Choice between all, rover or camera
 case "$1" in
 	all)
 	 camera
